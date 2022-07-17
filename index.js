c7d99298c68572b55ddc0db176e73398c71e6d58
Hassan KANSO <hassan.kanso@amadeus.com>
2022-07-17 21:58:21 +0200

    test continus automation

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-   res.send("Hello ya 3alam");
+   res.send("Hello ya 3alam. Let's rock it!");
 });
 
 
