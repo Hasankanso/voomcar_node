0c1574e2d9826078e61aad83c8c7f1e2dd23f709
Hassan KANSO <hassan.kanso@amadeus.com>
2022-07-17 19:10:46 +0200

    surrender, copy paste code

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var http = require('http');
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
-const normalizePort = require('normalize-port');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,17 +19,23 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-   res.send("Hello ya 3alam");
-});
-
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-var port = normalizePort(process.env.PORT || '80');
-app.set('port', port);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-http.createServer(app).listen(port);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
