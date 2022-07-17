9f7b951d8313050cd23f6e956f18110f65457f84
Hassan KANSO <hassan.kanso@amadeus.com>
2022-07-17 18:42:44 +0200

    Hello World

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 var http = require('http');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var app = express();
+const normalizePort = require('normalize-port');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(80);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+   res.send("Hello ya 3alam");
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+http.createServer(app).listen(port);
